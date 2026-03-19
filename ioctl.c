a94def5f82f4c90a6927f22079a01a28392732ff
david-bol12 <davidboland1001@gmail.com>
2026-03-19 15:46:44 +0000

    edited icotl

--- ioctl.c
+++ ioctl.c
@@ -11,22 +11,6 @@ long tablet_ioctl(struct file *file, unsigned int cmd, unsigned long arg) {
 
     switch (cmd) {
 
-    case TABLET_CLR_BUFFER:
-        cdev_buffer_clear();
-        return 0;
-
-    case TABLET_INJECT_EVENT:
-        if (copy_from_user(&event, (void __user *)arg, sizeof(event)))
-            return -EFAULT;
-        return cdev_buffer_write(&event);
-
-    case TABLET_GET_EVENT:
-        if (cdev_buffer_read(&event) < 0)
-            return -EAGAIN;
-        if (copy_to_user((void __user *)arg, &event, sizeof(event)))
-            return -EFAULT;
-        return 0;
-
     case TABLET_SET_BINDING:
         if (copy_from_user(&binding, (void __user *)arg, sizeof(binding)))
             return -EFAULT;
@@ -37,16 +21,8 @@ long tablet_ioctl(struct file *file, unsigned int cmd, unsigned long arg) {
         mutex_unlock(&bindings_mutex);
         return 0;
 
-    case TABLET_GET_BINDING:
-        if (copy_from_user(&binding, (void __user *)arg, sizeof(binding)))
-            return -EFAULT;
-        if (binding.button_id < 1 || binding.button_id > 10)
-            return -EINVAL;
-        mutex_lock(&bindings_mutex);
-        binding = button_bindings[binding.button_id];
-        mutex_unlock(&bindings_mutex);
-        if (copy_to_user((void __user *)arg, &binding, sizeof(binding)))
-            return -EFAULT;
+    case TABLET_GET_SETTING:
+
         return 0;
 
     case TABLET_CLR_BINDINGS:
